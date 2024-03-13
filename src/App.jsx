091120ac7e03a637043aa5d798d00489c0a7eb9a
CharlesLLM <charlesllm@orange.fr>
2024-03-13 14:31:35 +0100

    Store genres in browser DB
    Signed-off-by: CharlesLLM <charlesllm@orange.fr>

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
+import { useEffect } from "react";
 import List from "./components/List";
+import options from "./TMDB-wrapper";
+import { db } from "./db";
 
 function App() {
+  useEffect(() => {
+    fetch('https://api.themoviedb.org/3/genre/movie/list?language=en', options)
+      .then((res) => res.json())
+      .then(async (data) => {
+        if ((await db.genres.count()) !== data.genres.length) {
+          await db.genres.clear();
+          db.genres.bulkAdd(data.genres);
+        }
+      });
+  }, []);
+
   return (
     <>
       <h1 className="text-center">Movies</h1>
