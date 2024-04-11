6a2cb13aa2b9c1cc037cad716781c35a4aee2a07
CharlesLLM <charlesllm@orange.fr>
2024-04-11 23:56:42 +0200

    Min fix
    Signed-off-by: CharlesLLM <charlesllm@orange.fr>

--- src/TMDB-wrapper.js
+++ src/TMDB-wrapper.js
@@ -1,6 +1,6 @@
 const options = {
-  apiKey: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ZjI5NWEyNWQ2NTVlNGIyMjc1N2U0MDdjZThhMjUyZiIsInN1YiI6IjY1ZjE3NzkwMmZkZWM2MDE4OTIxOTE0MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0AzmkRMpkjUYf-ux_vX9o2meflI08vTnv43p_n-sR9g',
-  accountId: 21099410
+  apiKey: process.env.REACT_APP_TMDB_API_KEY,
+  accountId: process.env.REACT_APP_TMDB_ACCOUNT_ID,
 };
 
 export default options;
